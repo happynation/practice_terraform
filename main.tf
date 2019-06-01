67a21793840cba1798ae8bf669b55d382ccab8bc
vovatran1993 <vovatran1993@gmail.com>
2019-06-01 16:32:06 -0500

    ports added

--- main.tf
+++ main.tf
@@ -5,4 +5,8 @@ resource "docker_image" "image_id" {
 resource "docker_container" "container_id" {
     name = "blog"
     image = "${docker_image.image_id.latest}"
+    ports {
+        internal = "2368"
+        external = "80"
+    }
 }

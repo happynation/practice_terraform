35a7318639fc21a1c1a2dfff7ec3f4ce19d32236
happynation <rootvovak@gmail.com>
2023-06-13 17:47:43 -0500

    changed health check to 600

--- asg.tf
+++ asg.tf
@@ -3,6 +3,7 @@ resource "aws_autoscaling_group" "my_asg" {
   max_size                  = 5
   min_size                  = 2
   health_check_type         = "ELB"    # optional
+  health_check_grace_period = 600
   desired_capacity          = 2
   target_group_arns = [aws_lb_target_group.my_tg.arn]
 

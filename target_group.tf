d77c38a9cb05782232e93059d2e848f76f1f2b0b
happynation <rootvovak@gmail.com>
2023-06-13 20:17:56 -0500

    health check fixed

--- target_group.tf
+++ target_group.tf
@@ -8,9 +8,9 @@ resource "aws_lb_target_group" "my_tg" {
   health_check {
     path = "/"
     port = 80
-    healthy_threshold = 6
-    unhealthy_threshold = 5
-    timeout = 120
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
     interval = 300
     matcher = "200"  # has to be HTTP 200 or fails
   }

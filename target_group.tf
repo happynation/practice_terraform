055efb6713cd9f6e07bce15ace09a45e228c7ca1
happynation <rootvovak@gmail.com>
2023-06-13 19:05:21 -0500

    added health check

--- target_group.tf
+++ target_group.tf
@@ -4,4 +4,14 @@ resource "aws_lb_target_group" "my_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  
+  health_check {
+    path = "/"
+    port = 80
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    timeout = 120
+    interval = 300
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
 }
